6d7768dcaff91207214a851cf6468bbb81891b5b
Mikka <tmailb01@gmail.com>
2025-02-20 11:02:20 +0800

    Update main.py
    Testing object creation and bitwise addition of two positive integers

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from Number import Number as num
 from Binary_Ops import Binary_Ops as Bops
 
-x = int(input("Please enter a first number: "))
-y = int(input("Please enter second number: "))
+# This script is for testing the functionality of the Number and Binary_Ops classes.
 
-# Note a (and b respectively) do not hold the list directly
+# Prompt the user to enter two integers.
+x = int(input("Please enter the first number: "))
+y = int(input("Please enter the second number: "))
+
+# Convert the integers to their binary representation using the Number class.
+# Note: The objects a and b store instances of the Number class, not just the binary lists.
 a = num(x)
 b = num(y)
 
-print(f'Original: \n'
-      f'{a.bin_num}\n'
-      f'{b.bin_num}\n')
+# Display the original numbers and their binary representations.
+print(f'First number: {x}')
+print(a.num)  # Binary representation of x
+print(f'Second number: {y}')
+print(b.num)  # Binary representation of y
 
-result = Bops.addition_two_positive(a.bin_num,b.bin_num)
+# Perform binary addition using Binary_Ops.
+result = Bops.addition_two_positive(a.num, b.num)
 
-print(f'Result: {result}')
+# Display the result of the binary addition.
+print(f'\nResult:\n{result}')
